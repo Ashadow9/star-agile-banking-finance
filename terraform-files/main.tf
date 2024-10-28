ff7f61928d9bb0cd2d1760e9ea10b88516fbb23e
VARUNESH <130573730+Ashadow9@users.noreply.github.com>
2024-10-28 15:12:20 +0530

    Update main.tf

--- terraform-files/main.tf
+++ terraform-files/main.tf
@@ -1,24 +1,35 @@
 resource "aws_instance" "test-server" {
-  ami = "ami-0dee22c13ea7a9a67"
-  instance_type = "t2.micro"
-  key_name = "docker"
+  ami                    = "ami-0dee22c13ea7a9a67"
+  instance_type         = "t2.micro"
+  key_name               = "docker"
   vpc_security_group_ids = ["sg-0fc8b1c023e7b4e38"]
-   connection {
-     type = "ssh"
-     user = "ubuntu"
-     private_key = file("./docker.pem")
-     host = self.public_ip
-     }
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("./docker.pem")
+    host        = self.public_ip
+  }
+
+  # Provisioner to run commands on the instance
   provisioner "remote-exec" {
-     inline = ["echo 'wait to start the instance' "]
+    inline = [
+      "echo 'waiting for the instance to start...'",
+      "sleep 30"  # Wait for a while to ensure services are up
+    ]
   }
+
+  # Provisioner to create an inventory file for Ansible
+  provisioner "local-exec" {
+    command = "echo ${self.public_ip} > inventory"
+  }
+
+  # Provisioner to run the Ansible playbook
+  provisioner "local-exec" {
+    command = "ansible-playbook '/var/lib/jenkins/workspace/Banking and Finance Domain Project/terraform-files/ansibleplaybook.yml'"
+  }
+
   tags = {
-     Name = "test-server"
-     }
-  provisioner "local-exec" {
-     command = "echo ${aws_instance.test-server.public_ip} > inventory"
-     }
-  provisioner "local-exec" {
-     command = "ansible-playbook /var/lib/jenkins/workspace/Banking and Finance Domain Project/terraform-files/ansibleplaybook.yml"
-     }
+    Name = "test-server"
   }
+}
